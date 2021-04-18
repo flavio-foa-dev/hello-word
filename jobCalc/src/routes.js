88946f11a16b419d5f9f1220aa5ea6ed921635f6
Flavio Andrade <foa12381@gmail.com>
2021-04-18 15:56:34 -0300

    iniciando projetos

---
+++ jobCalc/src/routes.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const routes = express.Router();
+
+const views = __dirname + '/views'
+
+// request, response
+routes.get('/', (request, response) => response.render(views+'index'));
+routes.get('/job', (request, response) => response.render(views+'job'));
+routes.get('/job/edit', (request, response) => response.render(views+'job-edit'));
+routes.get('/profile', (request, response) => response.render(views+'profile'));
+
+
+
+module.exports = routes;
