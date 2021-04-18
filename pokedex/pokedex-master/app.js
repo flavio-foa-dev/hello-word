af88352b3053c247acba24a4ca6aa520ced09394
Flavio Andrade <foa12381@gmail.com>
2021-04-18 18:26:00 -0300

    exercicios de pokemon

---
+++ pokedex/pokedex-master/app.js
@@ -0,0 +1,30 @@
+const getPokemonUrl = (id) => `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+const generatePokemonPromises = () => Array(150).fill().map((_, index) =>
+  fetch(getPokemonUrl(index + 1)).then(response => response.json()));
+
+const fetchPokemon = () => {
+  const pokemomPromises = generatePokemonPromises()
+
+  Promise.all(pokemomPromises)
+    .then(pokemonsResolvidos => {
+      const lisPokemons = pokemonsResolvidos.reduce((accumulator, pokemon) => {
+        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+
+        accumulator += `
+          <li class="card ${types[0]}">
+            <img class="card-image" alt="${pokemon.name}" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png"/>
+            <h2 class="card-title">${pokemon.id}. ${pokemon.name}</h2>
+            <p class="card-subtitle">${types.join(' | ')}</p>
+          </li>
+        `
+
+        return accumulator
+      },'');
+
+      const ul =document.querySelector('[data-js="pokedex"]')
+      ul.innerHTML = lisPokemons
+    })
+}
+
+fetchPokemon();
