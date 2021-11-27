fe7099807594fc6529cb6409ecee3189fc135a59
Flavio Andrade <foa12381@gmail.com>
2021-11-27 16:20:37 -0300

    logica1

---
+++ logica/ford/logicaL.js
@@ -0,0 +1,21 @@
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+for(let i = 0;  lines.length > i; i+=1 ){
+
+  let x = Number(lines[i].split(" ")[0]);
+  let y = Number(lines[i].split(" ")[1]);
+  if(x <= 0 || y <= 0){break}
+  if(x > y) {
+    const a = x;
+    x = y;
+    y = a;
+  }
+  let sum = 0;
+  let string =''
+  for(let j = x; j<=y; j+=1){
+    string += j + ' ';
+    sum += j;
+  }
+  console.log(`${string}Sum=${sum}`);
+}
